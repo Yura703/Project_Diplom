3ca22757e2383d3c3737e53451a2a98419a12832
Yura703 <59707794+Yura703@users.noreply.github.com>
2021-02-11 21:47:25 +0300

    11.02.21

---
+++ EmployeeExam/Controllers/TitlesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmployeeExam.Data;
+using EmployeeExam.Data.Entities;
+
+namespace EmployeeExam.Controllers
+{
+    public class TitlesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TitlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Titles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Titles.ToListAsync());
+        }
+
+        // GET: Titles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var title = await _context.Titles
+                .FirstOrDefaultAsync(m => m.title_id == id);
+            if (title == null)
+            {
+                return NotFound();
+            }
+
+            return View(title);
+        }
+
+        // GET: Titles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Titles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("title_id,title_name,var_id,gr_security,interval")] Title title)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(title);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(title);
+        }
+
+        // GET: Titles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var title = await _context.Titles.FindAsync(id);
+            if (title == null)
+            {
+                return NotFound();
+            }
+            return View(title);
+        }
+
+        // POST: Titles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("title_id,title_name,var_id,gr_security,interval")] Title title)
+        {
+            if (id != title.title_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(title);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TitleExists(title.title_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(title);
+        }
+
+        // GET: Titles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var title = await _context.Titles
+                .FirstOrDefaultAsync(m => m.title_id == id);
+            if (title == null)
+            {
+                return NotFound();
+            }
+
+            return View(title);
+        }
+
+        // POST: Titles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var title = await _context.Titles.FindAsync(id);
+            _context.Titles.Remove(title);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TitleExists(int id)
+        {
+            return _context.Titles.Any(e => e.title_id == id);
+        }
+    }
+}
